5866d9983ae98da7b2f3407f8890a5a4af194274
Abdur-P-Rahim <bspirahim@gmail.com>
2024-01-15 23:50:15 +0600

    some changes

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 max-w-screen-lg mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -29,7 +29,7 @@ const Header = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn">Login</a>
                 </div>
             </div>
         </div>
