3b7b443f58919fd89b92d15b4bf8d6963eb5ba19
Abdur-P-Rahim <bspirahim@gmail.com>
2024-01-16 11:53:29 +0600

    resturant purpose section added

--- src/Pages/Home/Home/ResturantPurpose/ResturantPurpose.jsx
+++ src/Pages/Home/Home/ResturantPurpose/ResturantPurpose.jsx
@@ -4,9 +4,9 @@ const ResturantPurpose = () => {
         <div className="max-w-screen-lg mx-auto my-10">
             <div className="relative">
                 <img src="../../../../../public/home/chef-service.jpg" alt="" />
-                <div className="bg-black opacity-50 text-white p-16 text-center  w-3/4 mx-auto absolute bottom-14 left-32">
-                    <h3 className="text-white text-4xl">Bistro Boss</h3>
-                    <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, et harum inventore sed nam quisquam quod incidunt est pariatur reprehenderit?</p>
+                <div className="bg-black opacity-50 text-white md:p-16  text-center  w-3/4 mx-auto absolute md:bottom-14 md:left-32 bottom-0 left-12">
+                    <h3 className="text-white md:text-4xl">Bistro Boss</h3>
+                    <p className="md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, et harum inventore sed nam quisquam quod incidunt est pariatur reprehenderit?</p>
                 </div>
             </div>
         </div>
